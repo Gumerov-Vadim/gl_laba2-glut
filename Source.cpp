c6bbb28f8eb593cf0962a5257eff95a68826ca95
Gumerov-Vadim <95773619+Gumerov-Vadim@users.noreply.github.com>
2022-05-28 07:34:09 +0500

    first point

--- Source.cpp
+++ Source.cpp
@@ -7,43 +7,62 @@
 #include "math_3d.h"
 using namespace std;
 
-void changeViewPort(int w, int h)
+GLuint VBO;
+
+static void RenderSceneCB()
 {
-	glViewport(0, 0, w, h);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_POINTS, 0, 1);
+
+    glDisableVertexAttribArray(0);
+
+    glutSwapBuffers();
 }
 
-void RenderSceneCB()
+
+static void InitializeGlutCallbacks()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glutSwapBuffers();
+    glutDisplayFunc(RenderSceneCB);
 }
 
-int main(int argc, char* argv[]) {
+static void CreateVertexBuffer()
+{
+    Vector3f Vertices[1];
+    Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
 
-	glutInit(&argc, argv);  // Initialize GLUT
-	GLenum res = glewInit();
-	if (res != GLEW_OK)
-	{
-		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
-		return 1;
-	}
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);   // Set up some memory buffers for our display
-
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
 
 
-	glutInitWindowSize(1024, 768);
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Tutorial 01");
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 02");
 
-	//glutReshapeFunc(changeViewPort);  // Bind the two functions (above) to respond when necessary
+    InitializeGlutCallbacks();
 
-	glutDisplayFunc(RenderSceneCB);
+    // Must be done after glut is initialized!
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
 
-	// Very important!  This initializes the entry points in the OpenGL driver so we can call all the functions in the API.
-	//glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	glutMainLoop();
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-	glClear(GL_COLOR_BUFFER_BIT);
-	glutSwapBuffers();
-	return 0;
+    CreateVertexBuffer();
+
+    glutMainLoop();
+
+    return 0;
 }
